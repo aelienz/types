11054fd0cb6c5a3f610cfe675f9d25957df59e9b
mathletedev <mathletedev@gmail.com>
2021-07-02 13:27:43 -0700

    add player prop to playerentity

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -1,16 +1,19 @@
-import { Socket, Transform } from "../interfaces";
+import { Player, Socket, Transform } from "../interfaces";
 import { Entity } from "./entity";
 
 export class PlayerEntity extends Entity {
 	public readonly socket: Socket;
+	public readonly player: Player;
 
 	public constructor({
 		socket,
+		player,
 		ctx,
 		transform,
 		image
 	}: {
 		socket: Socket;
+		player: Player;
 		ctx: CanvasRenderingContext2D;
 		transform: Transform;
 		image: HTMLImageElement;
@@ -18,6 +21,7 @@ export class PlayerEntity extends Entity {
 		super({ ctx, transform, image });
 
 		this.socket = socket;
+		this.player = player;
 	}
 
 	public update() {}
