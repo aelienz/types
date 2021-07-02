37b33b428847d13fa40fdd1028ab6e47bde12775
mathletedev <mathletedev@gmail.com>
2021-07-02 13:15:20 -0700

    include socket in player

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -1,17 +1,23 @@
-import { Transform } from "../interfaces";
+import { Socket, Transform } from "../interfaces";
 import { Entity } from "./entity";
 
 export class PlayerEntity extends Entity {
+	public readonly socket: Socket;
+
 	public constructor({
+		socket,
 		ctx,
 		transform,
 		image
 	}: {
+		socket: Socket;
 		ctx: CanvasRenderingContext2D;
 		transform: Transform;
 		image: HTMLImageElement;
 	}) {
 		super({ ctx, transform, image });
+
+		this.socket = socket;
 	}
 
 	public update() {}
